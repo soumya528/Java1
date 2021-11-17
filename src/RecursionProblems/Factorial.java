48d8da7f57eb598c877589662e48c166eba09ebe
Sam528 <soumyamitra528@gmail.com>
2021-11-18 01:23:25 +0530

    Practise

---
+++ src/RecursionProblems/Factorial.java
@@ -0,0 +1,7 @@
+package RecursionProblems;
+
+public class Factorial {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
--- src/class1/WelcomeToJava.java
+++
@@ -1,8 +0,0 @@
-package class1;
-
-public class WelcomeToJava {
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
